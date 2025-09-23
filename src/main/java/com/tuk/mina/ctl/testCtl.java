0dffe70a1b82504d5c0f5483c37a733e29366cef
hyowon3434 <hyowon3434@naver.com>
2025-09-23 21:57:30 +0900

    first commit

---
+++ src/main/java/com/tuk/mina/ctl/testCtl.java
@@ -0,0 +1,14 @@
+package com.tuk.mina.ctl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testCtl {
+    
+    @GetMapping
+    public String test() {
+        return "Hello, World!";
+    }
+    
+}
