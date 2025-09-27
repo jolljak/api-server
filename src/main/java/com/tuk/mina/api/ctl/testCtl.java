718aa5765f7f78bf67fe31041d2e63322c826934
hyowon3434 <hyowon3434@naver.com>
2025-09-27 10:54:33 +0900

    fix: 디렉토리 구조 변경

--- src/main/java/com/tuk/mina/api/ctl/testCtl.java
+++ src/main/java/com/tuk/mina/api/ctl/testCtl.java
@@ -1,4 +1,4 @@
-package com.tuk.mina.ctl;
+package com.tuk.mina.api.ctl;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
