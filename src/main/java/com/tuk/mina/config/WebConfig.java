16eca175ee5a5b87800253cd142ad38d2d122cd1
han-hye-min <hhan010215@gmail.com>
2025-10-18 02:48:42 +0900

    fix: cors 중복 설정 수정

--- src/main/java/com/tuk/mina/config/WebConfig.java
+++ src/main/java/com/tuk/mina/config/WebConfig.java
@@ -12,7 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 1. 모든 경로에 대해 CORS 정책 적용
                 .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000") // 2. 허용할 출처(Origin) 지정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 3. 허용할 HTTP 메서드 지정
-                .allowedHeaders("*") // 4. 허용할 모든 HTTP 헤더 지정
-                .allowCredentials(true); // 5. 쿠키/인증 정보 포함 요청 허용
+                .allowedHeaders("*");// 4. 허용할 모든 HTTP 헤더 지정
     }
 }
