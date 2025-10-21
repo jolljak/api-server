d5f3715db1698d820e3ac1f76e03ec9a622d22a8
hyowon3434 <hyowon3434@naver.com>
2025-10-21 23:52:17 +0900

    feat: 파일 다운로드 API 구현

--- src/main/java/com/tuk/mina/api/common/fileCtl.java
+++ src/main/java/com/tuk/mina/api/common/fileCtl.java
@@ -1,24 +1,30 @@
 package com.tuk.mina.api.common;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.util.IOUtils;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/upload")
+@RequestMapping("/api/file")
 @Tag(name = "File Controller", description = "파일 관련 API")
 public class fileCtl {
 
@@ -31,7 +37,7 @@ public class fileCtl {
         this.amazonS3 = amazonS3;
     }
 
-    @PostMapping
+    @PostMapping("/upload")
     @Operation(summary = "file upload", description = "S3 파일 업로드")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
         try {
@@ -48,5 +54,28 @@ public class fileCtl {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
       }
+    }
+
+    @GetMapping("/download")
+    @Operation(summary = "file download", description = "S3 파일 다운로드")
+    public ResponseEntity<byte[]> download(@RequestParam("fileName") String fileName) {
+        try {
+            S3Object s3Object = amazonS3.getObject(bucket, fileName);
+            InputStream inputStream = s3Object.getObjectContent();
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            // headers.setContentDispositionFormData("attachment", fileName); // Forces download
+            headers.setContentLength(bytes.length);
+
+            return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (Exception e) { // Catch other S3 related exceptions
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
